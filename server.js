cad53a7648fb774ed77ff56b1e6ac8f3949988e2
AdinaParvez <81682886+AdinaParvez@users.noreply.github.com>
2023-07-12 13:31:26 +0500

    DevToProd

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const app = express();
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan("prod"));
 app.use(express.static(path.join(__dirname, "./client/build")));
 
 // Routes
