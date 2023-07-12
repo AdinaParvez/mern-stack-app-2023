f9f946413767dadbda552b154bc993245c0b7e4e
AdinaParvez <81682886+AdinaParvez@users.noreply.github.com>
2023-07-12 09:50:14 +0500

    3rdUpdate

--- server.js
+++ server.js
@@ -1,49 +1,48 @@
-//const express=require('express')
-//const colors=require('colors')
-import express from "express"; 
-import colors from "colors";
-import dotenv from "dotenv";
-import morgan from "morgan";
-import connectDB from "./config/db.js";
-import authRoutes from "./routes/authRoute.js"
-import categoryRoutes from './routes/categoryRoutes.js'
-import productRoutes from './routes/productRoutes.js'
-import cors from "cors"
-import path from 'path'
-import {fileURLToPath} from 'url'
-//configure env
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db.js");
+const authRoutes = require("./routes/authRoute.js");
+const categoryRoutes = require('./routes/categoryRoutes.js');
+const productRoutes = require('./routes/productRoutes.js');
+const cors = require("cors");
+const path = require('path');
+const { fileURLToPath } = require('url');
+
+// Configure env
 dotenv.config();
 
-
-//database config
+// Database config
 connectDB();
-//esmodulefix
+
+// ESM module fix
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//rest object
-const app=express();
+// Create express app
+const app = express();
 
-//middlewares
-app.use(cors())
-app.use(express.json())
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, './client/build')))
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')));
 
-//routes
-app.use('/api/v1/auth',authRoutes)
-app.use('/api/v1/category', categoryRoutes)
-app.use('/api/v1/product', productRoutes)
-//rest api
-app.use('*', function(req,res){
-  res.sendFile(path.join(new URL(import.meta.url).pathname, "../client/build/index.html"));
-})
+// Routes
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
 
-//PORT
-const PORT=process.env.PORT || 8080;
+// Serve static files
+app.use("*", function(req, res) {
+  res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
-//run listen
-app.listen(PORT,()=>{
+// PORT
+const PORT = process.env.PORT || 8080;
 
-    console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
-})
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
